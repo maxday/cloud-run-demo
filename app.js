f15365ae60182283093691b934816cb617397e63
maxday <maxime.david@datadoghq.com>
2022-05-09 13:54:22 -0400

    test

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dd-trace').init();
+const express = require('express');
+const app = express();
+
+app.get("/hello", async (req, res) => {
+  console.log("in /hello");
+  res.status(200).json({"msg":"Hello world!"});
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Application is now ready, listening to: ${port} randomId: ${Math.random()}`));
